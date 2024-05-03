372f74091dc89f61bc9bf4ac5d2ad1cd3c703ca9
Gerley Medina <jgmedina1991@gmail.com>
2024-05-03 00:44:00 -0500

    day Four

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,8 @@
 const User = require("./User")
 const Category = require("./Category")
 const Product = require("./Product")
+
+// realacion entre Product->categoryId
+
+Product.belongsTo(Category)
+Category.hasMany(Product)
---
+++ src/tests/product.test.js
@@ -0,0 +1,61 @@
+require("../models")
+const Category = require("../models/Category")
+const request = require("supertest")
+const app = require('../app')
+
+const BASE_URL = '/api/v1/products'
+let category
+
+let TOKEN
+
+beforeAll(async()=>{
+
+    const user  = {
+        email: "jaime12345@gmail.com",
+        password: "jaime12345"
+    }
+
+    const res = await request (app)
+        .post('/api/v1/users/login')
+        .send(user)
+
+    TOKEN = res.body.token
+
+    category = await Category.create({name:'tecnologia'})
+
+})
+
+
+test("POST -> BASE_URL, should return statusCode 201, and res.body.title === products.title", async ()=>{
+    
+    const product = {
+        title: "Celular",
+        description: "Motorola G40",
+        price:890,
+        categoryId: category.id
+    }
+
+    const res = await request(app)
+        .post(BASE_URL)
+        .send(product)
+        .set('Authorization', `Bearer ${TOKEN}`)
+    
+    expect(res.status).toBe(201)
+    expect(res.body).toBeDefined()
+    expect(res.body.title).toBe(product.title)
+
+    
+})
+
+test("GET -> BASE_URL, should return statusCode 200, and res.body===1", async()=>{
+
+    const res = await request(app)
+        .get(BASE_URL)
+
+    expect(res.status).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body).toHaveLength(1)
+
+    await category.destroy()
+})
+
