01e740ca26bfa95b5244ac05cb820ae7571385bd
Gerley Medina <jgmedina1991@gmail.com>
2024-05-03 01:14:37 -0500

    day Four.1

--- src/tests/product.test.js
+++ src/tests/product.test.js
@@ -1,12 +1,15 @@
 require("../models")
+
 const Category = require("../models/Category")
 const request = require("supertest")
 const app = require('../app')
 
 const BASE_URL = '/api/v1/products'
-let category
 
+let category
 let TOKEN
+let productId
+let product
 
 beforeAll(async()=>{
 
@@ -28,7 +31,7 @@ beforeAll(async()=>{
 
 test("POST -> BASE_URL, should return statusCode 201, and res.body.title === products.title", async ()=>{
     
-    const product = {
+    product = {
         title: "Celular",
         description: "Motorola G40",
         price:890,
@@ -40,6 +43,8 @@ test("POST -> BASE_URL, should return statusCode 201, and res.body.title === pro
         .send(product)
         .set('Authorization', `Bearer ${TOKEN}`)
     
+    productId = res.body.id
+
     expect(res.status).toBe(201)
     expect(res.body).toBeDefined()
     expect(res.body.title).toBe(product.title)
@@ -56,6 +61,19 @@ test("GET -> BASE_URL, should return statusCode 200, and res.body===1", async()=
     expect(res.body).toBeDefined()
     expect(res.body).toHaveLength(1)
 
-    await category.destroy()
+    
 })
 
+test('GET -> BASE_URL/:id, should return statusCode 201, and res.body.length === 1', async()=>{
+   
+    const res = await request(app)
+        .get(`${BASE_URL}/${productId}`)
+
+    
+    expect(res.status).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body.title).toBe(product.title)
+    
+    
+    await category.destroy()
+})
